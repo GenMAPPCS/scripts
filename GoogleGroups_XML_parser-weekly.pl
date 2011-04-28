bf7424e4691a5307e5af2df0ec565cc3ec25c644
Alexander Pico <apico@gladstone.ucsf.edu>
2011-04-28 02:26:08 +0000

    updated weekly report and readme

--- GoogleGroups_XML_parser-weekly.pl
+++ GoogleGroups_XML_parser-weekly.pl
@@ -51,27 +51,35 @@ close UTF;
         
 ## setup logfile and read in contents. 
 ## rather than appending, this method (reading / overwriting) allows for adding new content at the beginning of file.
-my $statsfile = $filename."-weekly.txt";
+my $statsfile = "/var/www/meta/trunk/nrnb/helpdesk.html";
 my $log = ""; #scalar to store existing contents of log file
 	
-unless ( open(LOGFILE, "<$statsfile") )   ## try to open existing file, if fail create new and open for output
+unless ( open(LOGFILE, "<$statsfile") )   ## open existing file, if fail create new and open for output
        {
 	   unless ( open(LOGFILE, ">$statsfile") )
        		{
         	print "opening new file $statsfile failed.\n";
         	exit;
        		}
+	  
        }
 
 else    ## read from existing file, then open the same file for output
 	{
-	read (LOGFILE, $log, 1000000);	
+	## store old log contents (excluding header)
+	while (<LOGFILE>) {
+		$log .= $_ unless 1 .. /^<hr>/;
+	}
 	unless ( open(LOGFILE, ">$statsfile") )
        		{
         	print "opening $statsfile for output failed.\n";
         	exit;
        		}	
 	}
+
+## print header
+print LOGFILE "<html><body><b><font size='+2'>Weekly report on unanswered helpdesk emails</font></b>\n";
+print LOGFILE "<br><a href=\"http://groups.google.com/group/cytoscape-helpdesk?hl=en_US\">cytoscape-helpdesk</a><br>\n";
 
 ## setup parser and specify input
 my $parser = XML::LibXML->new();
@@ -80,11 +88,13 @@ unlink($utffile); #delete temp file
 
 ## define date range: last week
 my $currenttime = time(); 		
-my $cutoff = $currenttime - '604800'; #define last week in epoch timestamp, 604800 is number of seconds in epoch format
+my $cutoff = $currenttime - 604800; #define last week in epoch timestamp, 604800 is number of seconds in epoch format
 my $startdate = localtime($cutoff);  ## switch format for readability
 my $enddate = localtime($currenttime);
-print LOGFILE "Unanswered email for the week of $startdate - $enddate\n";
-print LOGFILE "Topic\tAuthor\tDate\n";
+print LOGFILE "<hr>\n";
+print LOGFILE "<b>$startdate - $enddate</b><br>\n";
+print LOGFILE "<table cellspacing='10'>\n";
+print LOGFILE '<thead><tr><th>Topic</th><th>Author</th><th>Date</th></tr>'."\n";
 
 ## start parsing tree
 my $root = $dom->documentElement();  
@@ -95,6 +105,10 @@ foreach my $topic (@topics) {
 	## get topic title
 	my @title = $topic->getElementsByTagName('title');
 	my $title = $title[0]->textContent;
+
+        ## get link
+        my @link = $topic->getElementsByTagName('link');
+        my $link = $link[0]->textContent;
 	
 	## get number of posts in topic
     my @postselement = $topic->getElementsByTagName('posts');
@@ -109,7 +123,7 @@ foreach my $topic (@topics) {
 	## get author information
 	my @author = $firstpost->getElementsByTagName('author');
 	my $author = $author[0]->textContent;	
-	
+
 	## find and print unnswered posts in the last week
 	my @timestamp = $firstpost->getElementsByTagName('timestamp');
 	my $timestamp = $timestamp[0]->textContent;
@@ -122,12 +136,12 @@ foreach my $topic (@topics) {
 		if ($numposts == 1) 
 			{
 			my $time = localtime($timestamp);
-			print LOGFILE "$title\t$author\t$time\n";
+			print LOGFILE '<tr><td><a href="'.$link.'">'.$title.'</a></td><td>'.$author.'</td><td>'.$time.'</td></tr>'."\n";
 			}
 	}	
 	}
 }
-
-print LOGFILE "$log\n\n"; ## print old contents of logifle at the end of file
-print "\n\nDone\n\n";
+print LOGFILE "</table>\n";
+print LOGFILE "<hr>\n$log\n"; ## print old contents of logifle at the end of file
+print "Done\n";
 close LOGFILE;
