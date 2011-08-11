076b9b75e3fa2e2a5b90da69ed094da1b9ddd181
khanspers <khanspers@users.noreply.github.com>
2011-08-11 20:06:20 +0000

    added functionality for writing out a text file with GO term IDs and names

--- GO-parser/OBO-parser.pl
+++ GO-parser/OBO-parser.pl
@@ -1,10 +1,14 @@
  #!/usr/bin/perl
 
-## This script takes an obo file and creates a sif file.
-## For each GO term, any "is_a" term is recorded as an interaction.
+## This script takes an obo file and creates two files:
+## 1. sif file. For each GO term, any "is_a" term is recorded as an interaction.
 ## Input file name is hard-coded.
+## 2. text file listing GO term ID and GO term name
 ## 
-## Required input: GO obo file 
+## Required input: GO obo file, either from full or slim GO
+##
+## NOTE: input file names are hard-coded. Slim version is commneted out.
+## 
 
 #####################
 ## define input and output
@@ -18,8 +22,19 @@ unless ( open(OUTFILE, ">$sif") )
          exit;
  	}
 
+## define go structure outfile
+my $out = "gene-ontology.txt";
+#my $out = "gene-ontology_slim.txt";
+unless ( open(OUTFILE2, ">$out") )
+       {
+         print "could not open file $out\n";
+         exit;
+       }
+
+print OUTFILE2 "ID\tName";
+
 #Define input file
-my $inputfile = "gene-ontology.obo";
+my $inputfile = "gene_ontology_ext.obo";
 #my $inputfile = "goslim_generic.obo";
 my $input = "";
 my @input = ();
@@ -33,12 +48,16 @@ unless ( open(INPUT, $inputfile) )
 
 $input = join("", <INPUT>); ##read in as one line
 my $intA = "";
+my $ID = "";
+my $name = "";
+my %go = ();
 
 @input = split(/\[Term\]/, $input);
 
 foreach my $i (@input)
 { 
-	my @lines = split(/\n/, $i);
+	my $term = (split(/\[Typedef\]/,$i))[0];   ## remove extra info at the end of file
+	my @lines = split(/\n/, $term);
 	
 	foreach my $line (@lines)
 	{
@@ -50,18 +69,29 @@ foreach my $i (@input)
 			{
 			$line =~ /id:\s(GO:\d+)/;
 			$intA = $1;
+			$ID = $1;
+			}
+		elsif ($line =~ /name:\s.+/) 
+			{
+			$line =~ /name:\s(.+)/;
+			$name = $1;	
 			}
 		elsif ($line =~ /is_a:\sGO:\d+/)
 			{
 			$line =~ /is_a:\s(GO:\d+)/;	
 			my $intB = $1;
-			print OUTFILE "$intA\tis a\t$intB\n";
+			print OUTFILE "$intA\tis a\t$intB\n";  ## print sif file
 			}
-		
+		$go{$ID} = $name;
 	}
 }
 
+foreach my $k (sort keys %go)
+	{
+	print OUTFILE2 "$k\t$go{$k}\n";	  ## print text file
+	}
 close OUTFILE;
+close OUTFILE2;
 close INPUT;
 
 print "Done!\n\n";
